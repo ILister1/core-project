0c61980045b9a7a8513930990fab2dfc683303c7
ILister1 <ilister@qa.com>
2020-12-04 16:50:55 +0000

    testing script refinement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,13 +8,13 @@ sudo docker stack deploy --compose-file docker-compose.yaml core-project
 
 EOF
 
-ssh nginx-lb << EOF
+#ssh nginx-lb << EOF
 
-cd core-project
-git pull
-sudo docker stop nginx
-sudo docker rm nginx
-sudo docker rmi nginx:alpine
-. scripts/load_balancer.sh
+#cd core-project
+#git pull
+#sudo docker stop nginx
+#sudo docker rm nginx
+#sudo docker rmi nginx:alpine
+#. scripts/load_balancer.sh
 
-EOF
+#EOF
