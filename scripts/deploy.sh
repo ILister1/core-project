c4c345b1c4e0c1834ea0990dc523b71e1e80776c
ILister1 <ilister@qa.com>
2020-12-04 12:46:44 +0000

    automating nginx server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,3 +7,11 @@ git pull
 sudo docker stack deploy --compose-file docker-compose.yaml core-project
 
 EOF
+
+ssh nginx-lb << EOF
+
+sudo docker stop nginx
+sudo docker rm nginx
+. load_balancer.sh
+
+EOF
