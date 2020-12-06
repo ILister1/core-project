9f44d6ec5a265a8acfb3cbea0f763a82a4279bcf
ILister1 <ilister@qa.com>
2020-12-06 13:50:41 +0000

    dev branch second implementation test

--- service2/app.py
+++ service2/app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 @app.route('/theme', methods=['GET', 'POST'])
 def theme():
 
-    themes = ["Fortune found in", "Fears faced in", "Chance encounter in"]
-    return  Response(random.choice(themes), mimetype="text/plain")
+    #themes = ["Fortune found in", "Fears faced in", "Chance encounter in"]
+    #return  Response(random.choice(themes), mimetype="text/plain")
+
+     themes = ["Love found in", "Desire sensed in", "Death experienced in"]
+     return  Response(random.choice(themes), mimetype="text/plain")
         
- # implement a change here, alternate choice of themes?
+ 
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)    
+    app.run(debug=True, host='0.0.0.0', port=5001)        
