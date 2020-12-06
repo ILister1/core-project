13bad03f14c52f374cc10386c04f2b04ac007066
ILister1 <ilister@qa.com>
2020-12-06 16:05:07 +0000

    test2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,13 +8,4 @@ docker stack deploy --compose-file docker-compose.yaml core-project
 
 EOF
 
-ssh nginx-lb << EOF
 
-cd core-project
-git pull
-sudo docker stop nginx
-sudo docker rm nginx
-sudo docker rmi nginx:alpine
-. scripts/load_balancer.sh
-
-#EOF
