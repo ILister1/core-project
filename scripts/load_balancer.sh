5b28f3ecb352c5ec6d72e9efd01c78354caab43d
ILister1 <ilister@qa.com>
2020-12-06 19:23:42 +0000

    back to first version

--- scripts/load_balancer.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-  
-sudo docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
--- service2/app.py
+++ service2/app.py
@@ -8,8 +8,11 @@ def theme():
 
     themes = ["Fortune found in", "Fears faced in", "Chance encounter in"]
     return  Response(random.choice(themes), mimetype="text/plain")
+
+     #themes = ["Love found in", "Desire sensed in", "Death experienced in"]
+     #return  Response(random.choice(themes), mimetype="text/plain")
         
- # implement a change here, alternate choice of themes?
+ 
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)    
+    app.run(debug=True, host='0.0.0.0', port=5001)        
