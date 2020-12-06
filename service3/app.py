048a956c1fa81b5c6143abdf2a49f80e6f0a342c
ILister1 <ilister@qa.com>
2020-12-06 20:13:02 +0000

    dev-branch

--- service3/app.py
+++ service3/app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route('/setting', methods=['GET', 'POST'])
 def setting():
 
-    settings = ["a mysterious cavern", "an intimidating room", "a dreamlike headspace"]
-    return  Response(random.choice(settings), mimetype="text/plain")
+   # settings = ["a mysterious cavern", "an intimidating room", "a dreamlike headspace"]
+   # return  Response(random.choice(settings), mimetype="text/plain")
         
- # implement a change here, alternate choice of settings?
+    settings = ["an empty room", "a perfect place", "a second chance"]
+    return  Response(random.choice(settings), mimetype="text/plain")
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=5002)    
