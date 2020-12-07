65fff389636722b4bfb7d4663256c156024a261f
ILister1 <ilister@qa.com>
2020-12-07 08:48:03 +0000

    dev-branch

--- service2/app.py
+++ service2/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/theme', methods=['GET', 'POST'])
 def theme():
 
-    themes = ["Fortune found in", "Fears faced in", "Chance encounter in"]
-    return  Response(random.choice(themes), mimetype="text/plain")
+    #themes = ["Fortune found in", "Fears faced in", "Chance encounter in"]
+    #return  Response(random.choice(themes), mimetype="text/plain")
 
     
-    #themes = ["Love found in", "Desire sensed in", "Death experienced in"]
-    #return  Response(random.choice(themes), mimetype="text/plain")
+    themes = ["Love found in", "Desire sensed in", "Death experienced in"]
+    return  Response(random.choice(themes), mimetype="text/plain")
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)    
--- service3/app.py
+++ service3/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/setting', methods=['GET', 'POST'])
 def setting():
 
-    settings = ["a mysterious cavern", "an intimidating room", "a dreamlike headspace"]
-    return  Response(random.choice(settings), mimetype="text/plain")
-        
-    #settings = ["an empty room", "a perfect place", "a second chance"]
+    #settings = ["a mysterious cavern", "an intimidating room", "a dreamlike headspace"]
     #return  Response(random.choice(settings), mimetype="text/plain")
+        
+    settings = ["an empty room", "a perfect place", "a second chance"]
+    return  Response(random.choice(settings), mimetype="text/plain")
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=5002)    
